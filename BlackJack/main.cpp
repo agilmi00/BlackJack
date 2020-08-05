4c485802bb5dd10f8d43dbf5c655daa681d57744
Cl0ud7 <agilmi00@estudiantes.unileon.es>
2020-08-05 18:38:04 +0200

    Initial Commit

---
+++ BlackJack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BlackJack
+//
+//  Created by Adrián Gil Miranda on 05/08/2020.
+//  Copyright © 2020 Adrián Gil Miranda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
